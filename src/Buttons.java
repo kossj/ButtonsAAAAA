6b280ef74b69fd3bf83ccc9c73166d48e7c6dd5d
Jack Koss <jack@jackkoss.org>
2023-11-08 10:45:28 -0600

    initial

---
+++ src/Buttons.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Buttons {
+    public static void main(String[] args) {
+        new Buttons();
+    }
+
+    public Buttons() {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
